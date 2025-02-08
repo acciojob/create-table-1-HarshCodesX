b131a08ca26aaed9ea586c490f2a687d8070b6c5
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2025-02-08 18:24:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+	const Table = document.getElementById("sampleTable");
+	const newRow = document.createElement("tr");
+	const cellOne = document.createElement("td");
+	cellOne.innerHTML = "New Cell 1"
+	const cellTwo = document.createElement("td");
+	cellTwo.innerHTML = "New Cell 2"
+	newRow.appendChild(cellOne, cellTwo)
+	Table.appendChild(newRow);
   
 }
