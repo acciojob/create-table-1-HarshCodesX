894d0ed2936c2b61e4ca48f00d713546f3586de5
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2025-02-08 19:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,18 +19,16 @@
 
 
 
-window.onload = function() {
-    document.querySelector('input[type="button"]').addEventListener('click', insert_Row);
-}
-
 function insert_Row() {
     const Table = document.getElementById("sampleTable");
     const newRow = document.createElement("tr");
     const cellOne = document.createElement("td");
-    const cellTwo = document.createElement("td");
     cellOne.innerHTML = "New Cell1";
+    const cellTwo = document.createElement("td");
     cellTwo.innerHTML = "New Cell2";
     newRow.appendChild(cellOne);
     newRow.appendChild(cellTwo);
-    Table.appendChild(newRow);
+
+    // Insert the new row at the top of the table
+    Table.insertBefore(newRow, Table.firstChild);
 }
